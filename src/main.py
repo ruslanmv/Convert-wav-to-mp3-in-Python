4e82b07e63250c3ef05d250fb7e8aaf3e0c4553c
Ruslan Magana Vsevolodovna <51296249+ruslanmv@users.noreply.github.com>
2022-07-31 22:53:45 +0200

    update exe application
    update exe application

--- src/main.py
+++ src/main.py
@@ -1,39 +1,52 @@
+from os import path
+from pydub import AudioSegment
 from pathlib import Path
+from gooey import Gooey, GooeyParser
 import subprocess
 import platform
 
-from gooey import Gooey, GooeyParser
 
 
-@Gooey(dump_build_config=False, program_name="Audio to Video Conversion Tool")
+@Gooey(dump_build_config=False, program_name="wav to mp3 Conversion Tool - ruslanmv.com")
 def main():
-    desc = "A Python GUI App to convert a .mp3 and an image into a .mp4"
-    mp3_select_help_msg = "Select a .mp3 audio file to process"
-    image_select_help_msg = "Select an image file (.png or .jpg) to use in the video"
+    desc = "A Python GUI App to convert a .wav and  into a .mp3"
+    wav_select_help_msg = "Select a .wav audio file to process"
 
     my_parser = GooeyParser(description=desc)
     my_parser.add_argument(
-        "mp3_to_convert", help=mp3_select_help_msg, widget="FileChooser"
+        "wav_to_convert", help=wav_select_help_msg, widget="FileChooser"
     )
+
     my_parser.add_argument(
-        "image_to_convert", help=image_select_help_msg, widget="FileChooser"
-    )
-    my_parser.add_argument(
-        "output_dir", help="Directory to save output", widget="DirChooser"
+        "output_dir", help="Directory to save output mp3", widget="DirChooser"
     )
 
     args = my_parser.parse_args()
 
-    # construct the .mp3 input audio file path
-    mp3_to_convert_Path = Path(args.mp3_to_convert)
+    # construct the .wav input audio file path
+    wav_to_convert_Path = Path(args.wav_to_convert)
 
-    # construct image file path
-    image_to_convert_Path = Path(args.image_to_convert)
+    mp3_outfile_name = str(wav_to_convert_Path.stem) + ".mp3"
+    mp3_outfile_Path = Path(args.output_dir, mp3_outfile_name)
+    mp3_outfile_Path.unlink(missing_ok=True) # delete the .mp4 file if it's there
 
-    mp4_outfile_name = str(mp3_to_convert_Path.stem) + "_video.mp4"
-    mp4_outfile_Path = Path(args.output_dir, mp4_outfile_name)
-    mp4_outfile_Path.unlink(missing_ok=True) # delete the .mp4 file if it's there
 
+    print(f"input .wav file \n {wav_to_convert_Path}")
+    print()
+    print(f"output .mp3 file \n {mp3_outfile_Path}")
+    print()
+    
+    
+    #read wav file to an audio-segment
+    #song = AudioSegment.from_wav(wav_to_convert_Path) #your_wave_file.wav
+
+    #export audio segment to mp3
+    #song.export(mp3_outfile_Path, format="mp3")  #your_wave_file.mp3
+
+    #play audio-segment
+    #play(song)
+    
+    
     # Determine ffmpeg executable file path
     """
     where ffmpeg
@@ -47,32 +60,28 @@ def main():
 
     ffmpeg_executable_path = ffmpeg_path_bytes.decode().strip()
     print("ffmpeg_executable_path: ", ffmpeg_executable_path)
-
+    
+    '''
+    ffmpeg -i input.wav -vn -ar 44100 -ac 2 -b:a 192k output.mp3
+    '''
+    
     # create the ffmpeg command
-    """
-    ffmpeg -i image.jpg -i audio.mp3 out.mp3 (basic)
-
-    ffmpeg -loop 1 -framerate 1 -i image.jpg -i audio.mp3 -map 0:v -map 1:a -r 10 -vf "scale='iw-mod(iw,2)':'ih-mod(ih,2)',format=yuv420p" -movflags +faststart -shortest -fflags +shortest -max_interleave_delta 100M output.mp4
-    """
-    aux = '''-map 0:v -map 1:a -r 10 -vf "scale='iw-mod(iw,2)':'ih-mod(ih,2)',format=yuv420p" -movflags +faststart -shortest -fflags +shortest -max_interleave_delta 100M '''
-    ffmpeg_command = f"-loop 1 -framerate 1 -i {image_to_convert_Path} -i {mp3_to_convert_Path}  {aux}  {mp4_outfile_Path}"
+ 
+    aux = '-vn -ar 44100 -ac 2 -b:a 192k'
+    
+    ffmpeg_command = f"-i {wav_to_convert_Path} {aux} {mp3_outfile_Path}"
     cmd_command = f"{ffmpeg_executable_path} {ffmpeg_command}"
-
-
-    print(f"input .mp3 file \n {mp3_to_convert_Path}")
-    print()
-    print(f"input image file \n {image_to_convert_Path}")
-    print()
-    print(f"output .mp4 file \n {mp4_outfile_Path}")
-    print()
-    print("cmd prompt command: ")
-    print()
-  
-
+    
+    
     # call ffmpeg
     returned_value = subprocess.call(cmd_command, shell=True)# returns the exit code in unix
     
     print("returned value:", returned_value)
+    
+    
+    
+
+
 
 if __name__ == "__main__":
       main()
